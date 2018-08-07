d0a0b8665f227955e6c37b5c51fb83ea3dca45d4
salehjg <saleh_jamali@ymail.com>
2018-08-07 17:25:13 +0430

    fixed a bug in TGC formula for reduce_sum_4d.cu try04

--- kernels/cuda/reduce_sum_4d.cu
+++ kernels/cuda/reduce_sum_4d.cu
@@ -170,3 +170,196 @@ void reduce_sum_4d_try03(
             TGO
             );
 }
+
+
+///
+/// \param g_idata
+/// \param g_buff           Temporary global memory buffer
+/// \param g_odata
+/// \param dim0
+/// \param dim1
+/// \param dim2
+/// \param dim3
+/// \param overaxis0
+/// \param overaxis1
+/// \param overaxis2
+/// \param overaxis3
+/// \param TGC              Thread group count
+/// \param TGPB             Thread group per block
+/// \param SPT              Dim3 slices per thread
+/// \param TGO              Thread group offset
+__global__ void kernel_reduce_sum_4d_try04(
+        const float * __restrict__  g_idata,
+        float * __restrict__  g_buff,
+        float * __restrict__  g_odata,
+        const unsigned long dim0,
+        const unsigned long dim1,
+        const unsigned long dim2,
+        const unsigned long dim3,
+        const bool overaxis0,
+        const bool overaxis1,
+        const bool overaxis2,
+        const bool overaxis3,
+
+        const unsigned long TGC,
+        const unsigned long TGPB,
+        const unsigned long SPT,
+        const unsigned long TGO)
+{
+    extern __shared__ float smem_buff[];
+    if (overaxis0 && overaxis1 && overaxis2 && !overaxis3) {
+        unsigned long TPG = dim3; //threads per group
+        unsigned long GroupIndex = blockIdx.x * TGPB + threadIdx.x / TPG;
+        unsigned long _limit = dim0*dim1*dim2*dim3;
+        unsigned long GroupIndexWithinBlock = (GroupIndex - blockIdx.x *TGPB);
+
+        // Ignore incomplete groups at the end of each thread block.
+        if( GroupIndexWithinBlock < TGPB && GroupIndex < TGC){
+            //Thread Index In Thread Group
+            unsigned long TIITG = (threadIdx.x - (GroupIndexWithinBlock*TPG));
+            float thread_sum = 0.0; // sum for current thread in the thread group
+
+            //------------------------------------------------------------
+            for(unsigned long iSPT=0;iSPT<SPT;iSPT++){
+                unsigned long gidx =  TGO*GroupIndex + iSPT*dim3 + TIITG;
+                if(gidx < _limit ){
+
+                    //if(blockIdx.x==35)
+                    //    printf("bid: %06d, tid: %06d, GroupIndex: %06ld, ThreadIndexInGroup: %06ld, gidx: %06ld\n",
+                    //        blockIdx.x, threadIdx.x, GroupIndex, TIITG, gidx);
+
+                    thread_sum += g_idata[gidx];
+                }
+                else
+                {
+                    printf("** gidx: %ld, GroupIndex: %ld, sum: %f\n",gidx,GroupIndex,thread_sum);
+                }
+            }
+
+            //------------------------------------------------------------
+            // |---element0s---|-----element1s----|------....|
+            smem_buff[TIITG*TGPB + GroupIndexWithinBlock] = thread_sum;
+            if(GroupIndex==18225)
+                printf("bid: %06d, tid: %06d, GroupIndex: %06ld, ThreadIndexInGroup: %06ld, thread_sum: %f, smem_buff[%06ld]: %f \n",
+                       blockIdx.x, threadIdx.x, GroupIndex, TIITG, thread_sum, TIITG*TGPB + GroupIndexWithinBlock , smem_buff[TIITG*TGPB + GroupIndexWithinBlock]);
+
+
+        }
+
+        __syncthreads();
+
+        // parallel reduction of current block's shared memory buffer
+        unsigned int thid = threadIdx.x;
+        while(thid<TGPB){ // block stride loop
+            if(thid >= TGPB) break;
+            // -------------------
+            for(unsigned long stride=TGPB/2; stride>0; stride >>= 1){
+                if (thid < stride){
+                    for(int d3=0;d3<TPG;d3++)
+                        smem_buff[d3*TGPB + (thid)] += smem_buff[d3*TGPB + (thid+stride)];
+                }
+                __syncthreads();
+            }
+            // -------------------
+            thid += blockDim.x;
+        }
+
+        __syncthreads();
+
+        if(threadIdx.x==0) {
+            for (int d3 = 0; d3 < TPG; d3++) {
+                atomicAdd(&g_odata[d3], smem_buff[d3 * TGPB]);
+                //atomicAdd(&g_odata[d3], 1);
+                //if(blockIdx.x==35)printf("bid: %06d, thid: %06d, smem_buff[%06ld]: %f, g_odata[%06d]: %f\n",blockIdx.x, threadIdx.x, d3 * TGPB, smem_buff[d3 * TGPB], d3, g_odata[d3]);
+            }
+        }
+
+    }
+    //-------------------------------------------------------------------------------------------------------------
+    //
+    //
+    //مشکل یا در بخش پارالل ریداکشن است ولی چرا در هر دو حالت تیرای ۰۳ و ۰۴ که روش ها متفاوت هستند مشکل پا برجا بوده است؟
+    //
+    //مشکل یا در بخش آخر جمع کردن حاصل جمع ها هست ولی چطور اخه؟
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+    //
+
+
+
+
+
+
+
+
+}
+
+
+void reduce_sum_4d_try04(
+        float* g_idata,
+        float* g_odata,
+        unsigned long dim0,
+        unsigned long dim1,
+        unsigned long dim2,
+        unsigned long dim3,
+        bool overaxis0,
+        bool overaxis1,
+        bool overaxis2,
+        bool overaxis3)
+{
+    if( !(overaxis0 && overaxis1 && overaxis2 && !overaxis3) ) {
+        printf("ERROR @reduce_sum_4d_try01 --NOT IMPLEMENTED\n"); return;
+    }
+
+    unsigned long block = BLOCK_SIZE;
+
+
+    unsigned long SPT,TGC,TGO,TGPB, grid,TPG;
+
+    //Dim3 slice per thread
+    SPT = 10; //cte
+
+    //thread group offset
+    TGO = dim3 * SPT;
+
+    //thread group count
+    TGC = (unsigned long)((dim0*dim1*dim2+(SPT-1))/SPT);
+
+    //thread group per block
+    TGPB = (unsigned long)(BLOCK_SIZE / dim3);
+
+    //grid size
+    grid = ( TGC+(TGPB-1) ) / TGPB;
+
+    TPG = (unsigned long)dim3; //threads per group
+
+    printf("-------------------------------------------------------\n");
+    printf("KERNEL_GRID  : %ld\n", grid);
+    printf("KERNEL_BLOCK : %ld\n", block);
+    printf("KERNEL_SPT :   %ld\n", SPT);
+    printf("KERNEL_TGO :   %ld\n", TGO);
+    printf("KERNEL_TGC :   %ld\n", TGC);
+    printf("KERNEL_TGPB :  %ld\n", TGPB);
+
+    float* g_buffer;
+    CHECK(cudaMalloc((float**)&g_buffer, (TGC*TPG)*sizeof(float))); // ThreadGroupCount * ThreadsPerGroup
+    CHECK(cudaMemset(g_buffer,0,(TGC*TPG)*sizeof(float)));
+    kernel_reduce_sum_4d_try04 <<<grid, block, TGPB*TPG*sizeof(float)>>> (
+            g_idata, g_buffer, g_odata,
+                    dim0, dim1, dim2, dim3,
+                    overaxis0, overaxis1, overaxis2, overaxis3,
+                    TGC,
+                    TGPB,
+                    SPT,
+                    TGO
+    );
+}
